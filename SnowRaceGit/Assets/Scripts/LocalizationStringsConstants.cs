cf459bdc3ac12eceae6e3cd5157c921989fb0ea4
god1385 <80105405+god1385@users.noreply.github.com>
2023-11-22 23:32:41 +0300

    commit 2

--- SnowRaceGit/Assets/Scripts/LocalizationStringsConstants.cs
+++ SnowRaceGit/Assets/Scripts/LocalizationStringsConstants.cs
@@ -1,22 +1,15 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class LocalizationStringsConstants : MonoBehaviour
 {
-   
-    
     public event UnityAction<Launge> LaungeSeted;
 
     private const string LangKey = "LangKey";
 
-    
-    
     public static string _lang
     {
-        get { return PlayerPrefs.GetString(LangKey,null); }
+        get { return PlayerPrefs.GetString(LangKey, null); }
         private set { PlayerPrefs.SetString(LangKey, value); }
     }
     public static void SetLaunge(string lang)
@@ -25,4 +18,4 @@ public class LocalizationStringsConstants : MonoBehaviour
     }
 }
 
-public enum  Launge{ru,tr,en}
+public enum Launge { ru, tr, en }
